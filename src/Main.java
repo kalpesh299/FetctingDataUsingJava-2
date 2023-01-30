525a3d700873f524ac8c9eef55dcef04e26f0101
kalpesh patil <kalpesh123.kp78@gmail.com>
2023-01-30 13:30:04 +0530

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String url="https://api.chucknorris.io/jokes/random";
+
+        HttpRequest httpRequest=HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        HttpClient httpClient=HttpClient.newBuilder().build();
+        HttpResponse httpResponse=httpClient.send(httpRequest,HttpResponse.BodyHandlers.ofString());
+
+
+
+
+
+        System.out.println(httpResponse.body());
+
+    }
+}
